035558055ae7f0a67cb707b7a36cea8584c54bd2
Mike Clark <mike@clarkware.com>
2020-04-20 12:40:39 -0600

    Add LightLive

---
+++ lib/live_view_studio_web/live/light_live.ex
@@ -0,0 +1,57 @@
+defmodule LiveViewStudioWeb.LightLive do
+  use LiveViewStudioWeb, :live_view
+
+  def mount(_params, _session, socket) do
+    socket = assign(socket, :brightness, 10)
+    {:ok, socket}
+  end
+
+  def render(assigns) do
+    ~L"""
+    <h1>Front Porch Light</h1>
+    <div id="light">
+      <div class="meter">
+        <span style="width: <%= @brightness %>%">
+          <%= @brightness %>%
+        </span>
+      </div>
+
+      <button phx-click="off">
+        <img src="images/light-off.svg">
+      </button>
+
+      <button phx-click="down">
+        <img src="images/down.svg">
+      </button>
+
+      <button phx-click="up">
+        <img src="images/up.svg">
+      </button>
+
+      <button phx-click="on">
+        <img src="images/light-on.svg">
+      </button>
+    </div>
+    """
+  end
+
+  def handle_event("on", _, socket) do
+    socket = assign(socket, :brightness, 100)
+    {:noreply, socket}
+  end
+
+  def handle_event("up", _, socket) do
+    socket = update(socket, :brightness, &(&1 + 10))
+    {:noreply, socket}
+  end
+
+  def handle_event("down", _, socket) do
+    socket = update(socket, :brightness, &(&1 - 10))
+    {:noreply, socket}
+  end
+
+  def handle_event("off", _, socket) do
+    socket = assign(socket, :brightness, 0)
+    {:noreply, socket}
+  end
+end
--- lib/live_view_studio_web/router.ex
+++ lib/live_view_studio_web/router.ex
@@ -18,6 +18,7 @@ defmodule LiveViewStudioWeb.Router do
     pipe_through :browser
 
     live "/", PageLive
+    live "/light", LightLive
   end
 
   # Other scopes may use custom stacks.
