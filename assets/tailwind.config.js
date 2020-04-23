c37e6812d0213f7811949da8cd5c039e0b161a13
Mike Clark <mike@clarkware.com>
2020-04-23 10:39:57 -0600

    Remove fonts to fix Chrome 81 bug

--- assets/tailwind.config.js
+++ assets/tailwind.config.js
@@ -1,10 +1,25 @@
 module.exports = {
   theme: {
+    fontFamily: {
+      sans: [
+        // "system-ui",
+        "-apple-system",
+        // "BlinkMacSystemFont",
+        "Segoe UI",
+        "Roboto",
+        "Helvetica Neue",
+        "Arial",
+        "Noto Sans",
+        "sans-serif",
+        "Apple Color Emoji",
+        "Segoe UI Emoji",
+        "Segoe UI Symbol",
+        "Noto Color Emoji",
+      ],
+    },
   },
   variants: {
     //backgroundColor: ["responsive", "hover", "focus", "active"]
   },
-  plugins: [
-    require('@tailwindcss/ui')
-  ]
+  plugins: [require("@tailwindcss/ui")],
 };
