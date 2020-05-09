b5781f480b4a1f71d0eec7369382f32a98f53370
Matthew Boehlig <thetamind@gmail.com>
2020-05-09 11:36:58 -0500

    6. tutorial(sales_dashboard): Refresh stats with send_interval

--- lib/live_view_studio_web/live/sales_dashboard_live.ex
+++ lib/live_view_studio_web/live/sales_dashboard_live.ex
@@ -4,13 +4,30 @@ defmodule LiveViewStudioWeb.SalesDashboardLive do
   alias LiveViewStudio.Sales
 
   def mount(_params, _session, socket) do
-    socket =
-      assign(socket,
-        new_orders: Sales.new_orders(),
-        sales_amount: Sales.sales_amount(),
-        satisfaction: Sales.satisfaction()
-      )
+    if connected?(socket), do: :timer.send_interval(1000, self(), :tick)
+
+    socket = assign_stats(socket)
 
     {:ok, socket}
+  end
+
+  def handle_event("refresh", _, socket) do
+    socket = assign_stats(socket)
+
+    {:noreply, socket}
+  end
+
+  def handle_info(:tick, socket) do
+    socket = assign_stats(socket)
+
+    {:noreply, socket}
+  end
+
+  def assign_stats(socket) do
+    assign(socket,
+      new_orders: Sales.new_orders(),
+      sales_amount: Sales.sales_amount(),
+      satisfaction: Sales.satisfaction()
+    )
   end
 end
