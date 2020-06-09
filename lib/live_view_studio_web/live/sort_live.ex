d24803f338f2108ed4c3c83e0b0142ffdcd5459c
Mike Clark <mike@clarkware.com>
2020-06-09 16:58:10 -0600

    Finish SortLive

--- lib/live_view_studio_web/live/sort_live.ex
+++ lib/live_view_studio_web/live/sort_live.ex
@@ -11,12 +11,21 @@ defmodule LiveViewStudioWeb.SortLive do
     page = String.to_integer(params["page"] || "1")
     per_page = String.to_integer(params["per_page"] || "5")
 
+    sort_by = (params["sort_by"] || "id") |> String.to_atom()
+    sort_order = (params["sort_order"] || "asc") |> String.to_atom()
+
     paginate_options = %{page: page, per_page: per_page}
-    donations = Donations.list_donations(paginate: paginate_options)
+    sort_options = %{sort_by: sort_by, sort_order: sort_order}
+
+    donations =
+      Donations.list_donations(
+        paginate: paginate_options,
+        sort: sort_options
+      )
 
     socket =
       assign(socket,
-        options: paginate_options,
+        options: Map.merge(paginate_options, sort_options),
         donations: donations
       )
 
@@ -33,7 +42,9 @@ defmodule LiveViewStudioWeb.SortLive do
             socket,
             __MODULE__,
             page: socket.assigns.options.page,
-            per_page: per_page
+            per_page: per_page,
+            sort_by: socket.assigns.options.sort_by,
+            sort_order: socket.assigns.options.sort_order
           )
       )
 
@@ -44,16 +55,45 @@ defmodule LiveViewStudioWeb.SortLive do
     if Donations.almost_expired?(donation), do: "eat-now", else: "fresh"
   end
 
-  defp pagination_link(socket, text, page, per_page, class) do
+  defp pagination_link(socket, text, page, options, class) do
     live_patch(text,
       to:
         Routes.live_path(
           socket,
           __MODULE__,
           page: page,
-          per_page: per_page
+          per_page: options.per_page,
+          sort_by: options.sort_by,
+          sort_order: options.sort_order
         ),
       class: class
     )
   end
+
+  defp sort_link(socket, text, sort_by, options) do
+    text =
+      if sort_by == options.sort_by do
+        text <> emoji(options.sort_order)
+      else
+        text
+      end
+
+    live_patch(text,
+      to:
+        Routes.live_path(
+          socket,
+          __MODULE__,
+          sort_by: sort_by,
+          sort_order: toggle_sort_order(options.sort_order),
+          page: options.page,
+          per_page: options.per_page
+        )
+    )
+  end
+
+  defp toggle_sort_order(:asc), do: :desc
+  defp toggle_sort_order(:desc), do: :asc
+
+  defp emoji(:asc), do: "👇"
+  defp emoji(:desc), do: "👆"
 end
