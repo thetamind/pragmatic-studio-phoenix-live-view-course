6a18a5a4323db1e412d77b6e49e33e29a1dd1c39
Mike Clark <mike@clarkware.com>
2020-06-09 15:35:52 -0600

    Finish PaginateLive

--- lib/live_view_studio_web/live/paginate_live.ex
+++ lib/live_view_studio_web/live/paginate_live.ex
@@ -4,64 +4,56 @@ defmodule LiveViewStudioWeb.PaginateLive do
   alias LiveViewStudio.Donations
 
   def mount(_params, _session, socket) do
-    donations = Donations.list_donations()
-
-    socket =
-      assign(socket,
-        donations: donations
-      )
-
     {:ok, socket, temporary_assigns: [donations: []]}
   end
 
-  def render(assigns) do
-    ~L"""
-    <h1>Food Bank Donations</h1>
-    <div id="donations">
-      <div class="wrapper">
-        <table>
-          <thead>
-            <tr>
-              <th class="item">
-                Item
-              </th>
-              <th>
-                Quantity
-              </th>
-              <th>
-                Days Until Expires
-              </th>
-            </tr>
-          </thead>
-          <tbody>
-            <%= for donation <- @donations do %>
-              <tr>
-                <td class="item">
-                  <span class="id"><%= donation.id %></span>
-                  <%= donation.emoji %> <%= donation.item %>
-                </td>
-                <td>
-                  <%= donation.quantity %> lbs
-                </td>
-                <td>
-                  <span class="<%= expires_class(donation) %>">
-                    <%= donation.days_until_expires %>
-                  </span>
-                </td>
-              </tr>
-            <% end %>
-          </tbody>
-        </table>
-        <div class="footer">
-          <div class="pagination">
-          </div>
-        </div>
-      </div>
-    </div>
-    """
+  def handle_params(params, _url, socket) do
+    page = String.to_integer(params["page"] || "1")
+    per_page = String.to_integer(params["per_page"] || "5")
+
+    paginate_options = %{page: page, per_page: per_page}
+    donations = Donations.list_donations(paginate: paginate_options)
+
+    socket =
+      assign(socket,
+        options: paginate_options,
+        donations: donations
+      )
+
+    {:noreply, socket}
+  end
+
+  def handle_event("select-per-page", %{"per-page" => per_page}, socket) do
+    per_page = String.to_integer(per_page)
+
+    socket =
+      push_patch(socket,
+        to:
+          Routes.live_path(
+            socket,
+            __MODULE__,
+            page: socket.assigns.options.page,
+            per_page: per_page
+          )
+      )
+
+    {:noreply, socket}
   end
 
   defp expires_class(donation) do
     if Donations.almost_expired?(donation), do: "eat-now", else: "fresh"
+  end
+
+  defp pagination_link(socket, text, page, per_page, class) do
+    live_patch(text,
+      to:
+        Routes.live_path(
+          socket,
+          __MODULE__,
+          page: page,
+          per_page: per_page
+        ),
+      class: class
+    )
   end
 end
