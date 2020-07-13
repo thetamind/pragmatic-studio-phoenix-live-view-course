57ca4c48eba6b788d42f0425499f4e31199fb865
Matthew Boehlig <thetamind@gmail.com>
2020-07-13 02:12:02 -0500

    10. exercise(home): Table of Contents links to all examples

--- test/live_view_studio_web/live/page_live_test.exs
+++ test/live_view_studio_web/live/page_live_test.exs
@@ -8,4 +8,18 @@ defmodule LiveViewStudioWeb.PageLiveTest do
     assert disconnected_html =~ "Welcome to Phoenix!"
     assert render(page_live) =~ "Welcome to Phoenix!"
   end
+
+  test "table of contents", %{conn: conn} do
+    {:ok, view, _html} = live(conn, "/")
+    element(view, ".examples a", "Light") |> render_click()
+    assert_redirected(view, "/light")
+
+    {:ok, view, _html} = live(conn, "/")
+    element(view, ".examples a", "Team License") |> render_click()
+    assert_redirected(view, "/license")
+
+    {:ok, view, _html} = live(conn, "/")
+    element(view, ".examples a", "Filter") |> render_click()
+    assert_redirected(view, "/filter")
+  end
 end
