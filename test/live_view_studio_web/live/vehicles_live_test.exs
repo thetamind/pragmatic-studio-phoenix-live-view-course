46bd59e148f955d14d6f1ac64993d79f5102d668
Matthew Boehlig <thetamind@gmail.com>
2020-07-19 20:19:09 -0500

    11b. test(vehicles): Test vehicles show on page

---
+++ test/live_view_studio_web/live/vehicles_live_test.exs
@@ -0,0 +1,45 @@
+defmodule LiveViewStudioWeb.VehiclesLiveTest do
+  use LiveViewStudioWeb.ConnCase
+
+  import Phoenix.LiveViewTest
+
+  alias LiveViewStudio.Repo
+  alias LiveViewStudio.Vehicles
+  alias LiveViewStudio.Vehicles.Vehicle
+
+  setup [:fixtures]
+
+  test "shows vehicles", %{conn: conn, vehicles: vehicles} do
+    {:ok, view, _html} = live(conn, "/vehicles")
+
+    vehicle = vehicles |> List.first()
+
+    pattern = ~r/.*#{vehicle.make}.*#{vehicle.model}.*#{vehicle.color}.*/s
+
+    assert view |> element("#vehicles tbody tr", pattern) |> render() =~ pattern
+  end
+
+  defp fixtures(_context) do
+    alias LiveViewStudio.Repo
+
+    now = NaiveDateTime.utc_now() |> NaiveDateTime.truncate(:second)
+
+    vehicles =
+      for _i <- 1..100 do
+        %{
+          make: Faker.Vehicle.make(),
+          model: Faker.Vehicle.model(),
+          color: Faker.Color.name(),
+          inserted_at: now,
+          updated_at: now
+        }
+      end
+
+    {100, _} = Repo.insert_all(Vehicle, vehicles)
+
+    vehicles = Repo.all(Vehicle)
+    assert Enum.count(vehicles) == 100
+
+    [vehicles: vehicles]
+  end
+end
