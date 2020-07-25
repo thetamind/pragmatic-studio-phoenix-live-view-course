7471060237970383dd5e2309088a5ef0c0aa207f
Matthew Boehlig <thetamind@gmail.com>
2020-07-25 14:26:52 -0500

    11b. test(vehicles): Extract table data using DOM

--- test/live_view_studio_web/live/vehicles_live_test.exs
+++ test/live_view_studio_web/live/vehicles_live_test.exs
@@ -2,6 +2,7 @@ defmodule LiveViewStudioWeb.VehiclesLiveTest do
   use LiveViewStudioWeb.ConnCase
 
   import Phoenix.LiveViewTest
+  alias Phoenix.LiveViewTest.DOM
 
   alias LiveViewStudio.Repo
   alias LiveViewStudio.Vehicles.Vehicle
@@ -18,16 +19,47 @@ defmodule LiveViewStudioWeb.VehiclesLiveTest do
     assert view |> element("#vehicles tbody tr", pattern) |> render() =~ pattern
   end
 
-  test "shows first page of 10 vehicles", %{conn: conn} do
+  test "shows first page of 10 vehicles", %{conn: conn, vehicles: vehicles} do
     {:ok, view, _html} = live(conn, "/vehicles")
 
-    html = view |> element("#vehicles table") |> render()
+    assert vehicle_count(view) == 10
 
-    assert Floki.parse_fragment!(html)
-           |> Floki.find("tbody")
-           |> List.first()
-           |> Floki.children()
-           |> Enum.count() == 10
+    expected_ids = vehicles |> Enum.take(10) |> Enum.map(& &1.id)
+    assert vehicle_ids(view) == expected_ids
+  end
+
+  defp vehicle_ids(view) do
+    view
+    |> table_data()
+    |> Enum.map(fn row ->
+      List.first(row) |> String.to_integer()
+    end)
+  end
+
+  defp vehicle_count(view) do
+    view
+    |> table_data()
+    |> Enum.count()
+  end
+
+  defp table_data(view) do
+    view
+    |> element("#vehicles table")
+    |> render()
+    |> DOM.parse()
+    |> DOM.all("tbody tr")
+    |> Enum.map(&extract_table_row/1)
+  end
+
+  defp extract_table_row({"tr", _attrs, children}) do
+    children
+    |> Enum.map(fn row ->
+      row
+      |> Floki.children()
+      |> List.first()
+      |> String.split([" ", "\n"], trim: true)
+      |> List.first()
+    end)
   end
 
   defp fixtures(_context) do
