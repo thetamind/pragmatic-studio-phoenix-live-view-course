27d3f749766100def8226498f5eaba093b0386fe
Matthew Boehlig <thetamind@gmail.com>
2020-07-28 03:19:16 -0500

    chore: Pull a couple things from Phoenix master

--- lib/live_view_studio_web/telemetry.ex
+++ lib/live_view_studio_web/telemetry.ex
@@ -31,11 +31,26 @@ defmodule LiveViewStudioWeb.Telemetry do
       ),
 
       # Database Metrics
-      summary("live_view_studio.repo.query.total_time", unit: {:native, :millisecond}),
-      summary("live_view_studio.repo.query.decode_time", unit: {:native, :millisecond}),
-      summary("live_view_studio.repo.query.query_time", unit: {:native, :millisecond}),
-      summary("live_view_studio.repo.query.queue_time", unit: {:native, :millisecond}),
-      summary("live_view_studio.repo.query.idle_time", unit: {:native, :millisecond}),
+      summary("live_view_studio.repo.query.total_time",
+        unit: {:native, :millisecond},
+        description: "The sum of the other measurements"
+      ),
+      summary("live_view_studio.repo.query.decode_time",
+        unit: {:native, :millisecond},
+        description: "The time spent decoding the data received from the database"
+      ), 
+      summary("live_view_studio.repo.query.query_time",
+        unit: {:native, :millisecond},
+        description: "The time spent executing the query"
+      ),
+      summary("live_view_studio.repo.query.queue_time",
+        unit: {:native, :millisecond},
+        description: "The time spent waiting for a database connection"
+      ),
+      summary("live_view_studio.repo.query.idle_time",
+        unit: {:native, :millisecond},
+        description: "The time the connection spent waiting before being checked out for the query"
+      ),
 
       # VM Metrics
       summary("vm.memory.total", unit: {:byte, :kilobyte}),
--- mix.exs
+++ mix.exs
@@ -67,7 +67,7 @@ defmodule LiveViewStudio.MixProject do
       setup: ["deps.get", "ecto.setup", "cmd npm install --prefix assets"],
       "ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
       "ecto.reset": ["ecto.drop", "ecto.setup"],
-      test: ["ecto.create --quiet", "ecto.migrate", "test"]
+      test: ["ecto.create --quiet", "ecto.migrate --quiet", "test"]
     ]
   end
 end
