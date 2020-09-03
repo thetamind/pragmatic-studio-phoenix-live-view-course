704e5dea363e3b6b17b6ffd128de8a19eae22f57
Matthew Boehlig <thetamind@gmail.com>
2020-09-03 16:36:12 -0500

    build: Update dep floki to 0.28

--- mix.exs
+++ mix.exs
@@ -38,7 +38,7 @@ defmodule LiveViewStudio.MixProject do
       {:ecto_sql, "~> 3.4 and >= 3.4.4"},
       {:postgrex, ">= 0.0.0"},
       {:phoenix_live_view, "~> 0.14.1"},
-      {:floki, ">= 0.0.0", only: :test},
+      {:floki, "~> 0.28", only: :test},
       {:phoenix_html, "~> 2.11"},
       {:phoenix_live_reload, "~> 1.2", only: :dev},
       {:phoenix_live_dashboard, "~> 0.2"},
