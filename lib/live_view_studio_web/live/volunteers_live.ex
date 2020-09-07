9bb383b84659574be5757d63945d8f98d707c31f
Matthew Boehlig <thetamind@gmail.com>
2020-09-07 02:43:27 -0500

    create with invalid inputs shows errors

--- lib/live_view_studio_web/live/volunteers_live.ex
+++ lib/live_view_studio_web/live/volunteers_live.ex
@@ -7,11 +7,30 @@ defmodule LiveViewStudioWeb.VolunteersLive do
   def mount(_params, _session, socket) do
     volunteers = Volunteers.list_volunteers()
 
+    changeset = Volunteers.change_volunteer(%Volunteer{})
+
     socket =
       assign(socket,
+        changeset: changeset,
         volunteers: volunteers
       )
 
     {:ok, socket}
+  end
+
+  def handle_event("save", %{"volunteer" => params}, socket) do
+    socket =
+      case Volunteers.create_volunteer(params) do
+        {:ok, volunteer} ->
+          socket = assign(socket, volunteers: [volunteer])
+          changeset = Volunteers.change_volunteer(%Volunteer{}, params)
+
+          assign(socket, changeset: changeset)
+
+        {:error, changeset} ->
+          assign(socket, changeset: changeset)
+      end
+
+    {:noreply, socket}
   end
 end
---
+++ test/live_view_studio_web/live/volunteers_live_test.exs
@@ -0,0 +1,37 @@
+defmodule LiveViewStudioWeb.VolunteersLiveTest do
+  use LiveViewStudioWeb.ConnCase
+
+  import Phoenix.LiveViewTest
+  alias Phoenix.LiveViewTest.DOM
+
+  alias LiveViewStudio.Repo
+  alias LiveViewStudio.Volunteers
+  alias LiveViewStudio.Volunteers.Volunteer
+  alias LiveViewStudioWeb.VolunteersLive
+
+  @valid_attrs %{name: "My Name", phone: "111-555-1234"}
+  @invalid_attrs %{name: "a", phone: "12-12-12"}
+
+  describe "create with valid inputs" do
+    test "shows in list"
+    test "prepends to list"
+  end
+
+  describe "create with invalid inputs" do
+    test "shows errors", %{conn: conn} do
+      {:ok, view, _html} = live(conn, "/volunteers")
+
+      view |> form("form", volunteer: @invalid_attrs) |> render_submit()
+
+      assert view
+             |> element(".field span[phx-feedback-for=volunteer_name]")
+             |> render() =~
+               "should be at least"
+
+      assert view |> element(".field .invalid-feedback", "phone") |> render() =~
+               "must be a valid phone number"
+    end
+
+    test "does not show in list"
+  end
+end
