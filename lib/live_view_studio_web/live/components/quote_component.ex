8754bdd39c518e370e857e72adfe254123cdbaa8
Matthew Boehlig <thetamind@gmail.com>
2020-10-01 04:40:36 -0500

    18. tutorial(sandbox): Quote is a stateless component

---
+++ lib/live_view_studio_web/live/components/quote_component.ex
@@ -0,0 +1,26 @@
+defmodule LiveViewStudioWeb.QuoteComponent do
+  use LiveViewStudioWeb, :live_component
+
+  import Number.Currency
+
+  def mount(socket) do
+    socket = assign(socket, hrs_until_expires: 24)
+    {:ok, socket}
+  end
+
+  def render(assigns) do
+    ~L"""
+    <div class="text-center p-6 border-4 border-dashed border-indigo-600">
+      <h2 class="text-2xl mb-2">
+        Our Best Deal:
+      </h2>
+      <h3 class="text-xl font-semibold text-indigo-600">
+        <%= @weight %> pounds of <%= @material %> for <%= number_to_currency(@price) %>
+      </h3>
+      <div class="text-gray-600">
+        expires in <%= @hrs_until_expires %> hours
+      </div>
+    </div>
+    """
+  end
+end
--- lib/live_view_studio_web/live/sandbox_live.ex
+++ lib/live_view_studio_web/live/sandbox_live.ex
@@ -1,6 +1,8 @@
 defmodule LiveViewStudioWeb.SandboxLive do
   use LiveViewStudioWeb, :live_view
 
+  alias LiveViewStudioWeb.QuoteComponent
+
   def mount(_params, _session, socket) do
     {:ok, socket}
   end
@@ -10,6 +12,11 @@ defmodule LiveViewStudioWeb.SandboxLive do
     <h1>Build A Sandbox</h1>
 
     <div id="sandbox">
+      <%= live_component @socket, QuoteComponent,
+          material: "sand",
+          weight: 10.0,
+          price: 15.0
+      %>
     </div>
     """
   end
