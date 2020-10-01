a047f785398624d42da8e7bcecf92b27b0956c08
Matthew Boehlig <thetamind@gmail.com>
2020-10-01 05:01:29 -0500

    18. tutorial(sandbox): Sandbox calculator is a stateful component

---
+++ lib/live_view_studio_web/live/components/sandbox_calculator_component.ex
@@ -0,0 +1,48 @@
+defmodule LiveViewStudioWeb.SandboxCalculatorComponent do
+  use LiveViewStudioWeb, :live_component
+
+  alias LiveViewStudio.SandboxCalculator
+
+  def mount(socket) do
+    {:ok, assign(socket, length: nil, width: nil, depth: nil, weight: 0)}
+  end
+
+  def render(assigns) do
+    ~L"""
+    <form phx-change="calculate" phx-target="<%= @myself %>"
+          phx-submit="get-quote">
+      <div class="field">
+        <label for="length">Length:</label>
+        <input type="number" name="length" value="<%= @length %>" />
+        <span class="unit">feet</span>
+      </div>
+      <div class="field">
+        <label for="length">Width:</label>
+        <input type="number" name="width" value="<%= @width %>" />
+        <span class="unit">feet</span>
+      </div>
+      <div class="field">
+        <label for="length">Depth:</label>
+        <input type="number" name="depth" value="<%= @depth %>" />
+        <span class="unit">inches</span>
+      </div>
+      <div class="weight">
+        You need <%= @weight %> pounds
+      </div>
+      <button type="submit">
+        Get Quote
+      </button>
+    </form>
+    """
+  end
+
+  def handle_event("calculate", params, socket) do
+    %{"length" => l, "width" => w, "depth" => d} = params
+
+    weight = SandboxCalculator.calculate_weight(l, w, d)
+
+    socket = assign(socket, length: l, width: w, depth: d, weight: weight)
+
+    {:noreply, socket}
+  end
+end
--- lib/live_view_studio_web/live/sandbox_live.ex
+++ lib/live_view_studio_web/live/sandbox_live.ex
@@ -2,6 +2,7 @@ defmodule LiveViewStudioWeb.SandboxLive do
   use LiveViewStudioWeb, :live_view
 
   alias LiveViewStudioWeb.QuoteComponent
+  alias LiveViewStudioWeb.SandboxCalculatorComponent
 
   def mount(_params, _session, socket) do
     {:ok, socket}
@@ -12,6 +13,9 @@ defmodule LiveViewStudioWeb.SandboxLive do
     <h1>Build A Sandbox</h1>
 
     <div id="sandbox">
+      <%= live_component @socket, SandboxCalculatorComponent,
+          id: 1
+      %>
       <%= live_component @socket, QuoteComponent,
           material: "sand",
           weight: 10.0,
